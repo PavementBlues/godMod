db2d36780746a70b3caa4293e4e67681178dff59
= <brenhanrahan@gmail.com>
2016-02-04 21:52:11 -0800

    Registered models in admin.py.

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+mainAppModels = [Subreddit, Redditor, SubModerators, Post, Comment,
+	CommentResponses, User, Discussion, Report]
+admin.site.register(mainAppModels)
